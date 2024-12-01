99bffb0675fb37bb38c67e6e8ed0f12763be31b6
NEMOmeovv <NEMOmeovv@gmail.com>
2024-12-02 08:00:26 +0900

    HTML 템플릿 불러옴

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from models.db_access import DB_Access
 
 app = Flask(__name__)
 
+@app.route('/home')
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/student/info')
 def search_student():
@@ -21,11 +22,10 @@ def check_student():
         print(result)
         return render_template('show_info.html',std_infos=result)
 
-@app.route('/test')
+@app.route('/no-sidebar')
 def test():
-    return render_template('index.html')
+    return render_template('no-sidebar.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 # TODO: 학생 정보에 info_release 라는 이름의 공개 여부bool 타입으로 정의
